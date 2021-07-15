cebb6e83efa653d64d184f1f86bbf7ca1390434e
michael ameteku <Ameteku@github.com>
2021-07-15 02:48:41 -0400

    Add endpoints for vehicl info call from frontend

--- server.js
+++ server.js
@@ -1,55 +1,124 @@
-
-/* functions of the server,
-1. Take login credentials and return user object,
-2. Take in api credentials and authorize access
-3. Once login anf access is granted, return vehicle info object
-4. Send request for locking, unlocking, reporting and notifying driver
-5. log out/invaliddate session
-*/
-
-
-const https = require('https');
-const express = require("express")
+const express = require("express");
 const request = require("request");
 
 
 const app = express();
 
-app.get('/', () =>{
+const applicationId = "afdc085b-377a-4351-b23e-5e1d35fb3700";
+const clientId = '30990062-9618-40e1-a27b-7c6bcb23658a';
+const clientSecret = 'T_Wk41dx2U9v22R5sQD4Z_E1u-l2B-jXHE';
+const code = 'eyJraWQiOiI2cjIzQ2FTeTF4cFdUUFBxYVRtX01Vc2RKZGo1RWlDTnRtME4yVTAxNTdFIiwidmVyIjoiMS4wIiwiemlwIjoiRGVmbGF0ZSIsInNlciI6IjEuMCJ9.AuVaDZOmWNPVxIzVhoMjxuohY2fk57XDt3aV7J77ucRec7foeJ5b0LpD4tkYK3idg_I1rbVmTYVwZUc4ErbGT2rw4o4tk0ojsXJfLcXlG2jzh1KFBELLrisVWub7d7Q0fXconYv4lQasblE8USMWHXXep0evaKzVV5gJn0ObusFem8xTN0hOK7NdbhC18izkHUvHe931590N24RomSLym3-IQFPQBc3moGHDp4_CxjfwhLb-BdgoVVS3fsvOmBvV8BpXg0fqi3kXW9MXXo0JCHoPWREJmilVcySh7Qczql_ITVJK1AOjKRjrLVoAkSCgIyJ_8lkcliUCPOGe5itjZw.Gai5ATNWQa2q0HmI.u62AVkXnwPRFhh6ZW2YUBiAdB-Vsp6NpdVKBX3eS_ivazxILU_fqwetsNHxAXGKOhaYacHkgcVjezckCncem8OobcSa-cd1O43nYYY2RurD_JwGxtr_JD2fWk11_VxV0aK05IdBa-e8HcJdt-_sXotETyMvBrCSpVdunRpdgkMCByTteVzy0zzXehmHeXWND5x-K9bEqGJG6pDRPfQYE2_9kj1c1g6J9gHcg2m_7d9oQ6Tj2hmnu_eNIbZwPihKnZEo2d8YhxPETLrC9h-fQmL0RxTquQpX7yX_vxgbRw8SriyttunlO_fnJs_OZYS3JXmy3JYXle2g7w2R02AVm1wqhx5gLU4EFLyXLxblV9bMo6HeMJmLnTDm51wjWHUrrm_6eP1bAGd7RDKA_tmaQJibxBb9WWQ2kVvBvolReRT5n6R5ed3GVWCvoM20AyT1wwYAxLLUeyW2gdxbTaCL8Wge6I_VwYIMXc4rYQozXM4_D0QWDXAcivmaiL8NonW5zSWFAEjmcSCPxPJ4sutBtHFzKYVYOnEPDL8pfgbP9OSTsHdeJF2Xvz9gIdHSz1U-c_dCR-7NQ5y-FSybrAmoW6LknG2JZtPmVr1WcikxrC9nR-44xDbbgJ6B72s_hkRPpfLFvl9ocWGPGbJ-mhNouS7-umvaLE7ON3ao215KkY49roHEcYAXQgSDZHx9-6u4a3v51TXSoBr-CzxYVnHSZUZ8-I6K4q4hJZRV3R53Zb17CQ1Aj4g0AWKVcrM-kA-HnA5tJyog6YbKTwT1-VqtLVbW8OoW_DkFPghxgkL7ifOo8f4EtSZn6OY1sDpjE8DQ19txVLQGDvd2O.C2U1NwtVUcwtaCNDoP_wsg';
+let accessToken = '';
+// app.get('/oauth/redirect', (req, res) => {
+//   console.log("in here");
+//   console.log(req.query.code);
 
-})
+// })
 
-//code for getting the access_token from ford
-app.get("/get_token", (req, res) => {
 
-  var options = {
-    'method': 'POST',
-      'url': 'https://dah2vb2cprod.b2clogin.com/914d88b1-3523-4bf6-9be4-1b96b4f6f919/oauth2/v2.0/token?p=B2C_1A_signup_signin_common',
-      'headers': {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      form: {
+const accessTokenOptions =  {
+  'method': 'POST',
+  'url': 'https://dah2vb2cprod.b2clogin.com/914d88b1-3523-4bf6-9be4-1b96b4f6f919/oauth2/v2.0/token?p=B2C_1A_signup_signin_common',
+  'headers': {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  form: {
     'grant_type': 'authorization_code',
-    'client_id': '30990062-9618-40e1-a27b-7c6bcb23658a',
-    'client_secret': 'T_Wk41dx2U9v22R5sQD4Z_E1u-l2B-jXHE',
-    'code': 'eyJraWQiOiI2cjIzQ2FTeTF4cFdUUFBxYVRtX01Vc2RKZGo1RWlDTnRtME4yVTAxNTdFIiwidmVyIjoiMS4wIiwiemlwIjoiRGVmbGF0ZSIsInNlciI6IjEuMCJ9.HZJfhdrvkwjacTmQvsHCDUUSDLy2qSQCjFoTROvX-EmiUAHSmINeewanoz3IWGbpPGfRQkbkaL0L1C5V_a0HRgaFoYOXWTAsChuMSmIZqzFbuSvbPJJRKJjLLKRcILS814xjLOsq401QBOHtvG75RSRmFLui0hXFgXNYC9-kWQ8Cua0xJIvUUzNdjpgx5mp9gDyNBB6B-sERatEBnN6Hv44slKqRkyE8P_Z5vaf8NTVTAgQba-R_6GzZGUalf-O84QbrHLPPHP9mMjePp-tG0KjIWg7xtsDBetWjx78OlOqu6X7vfpuYBweITP4v-IXRzIZQtQ1vLBIuyGzTud1X6g.yquJYJ_yp0z-B_Dy.IX31Y0oSNCvh-GJJnZhi0ZF-Nl3dH3WW0S4G1sqCiXetbIpq3SLovaj971q1JoCbfbyaPFzculpsXngNIKdiuHF8fFiGCM_WG9Fcseaa4XEopkgqxvCNjsd2f-LnUzruKKfICDBMMHoAaeC1u1i9Az7OgPm1IiSuf5iSyrkpbwisAFGglQmL13HJoZWGx350QoONy4rMMIG6ivaTvEUvWn9MeyFLsF9mV_MRRXxAVPBpZqC6XM4coEe4OGxz5tB9p9KvZk8q7a9Vdx_awPXLhUok_JBnh3dpzLuTJsP9JzoA5OahRfibmNBveiY_wP3I8EXgrd7D2w4MEZ_mSfnYvrHC555jy1v3v34oMk_HMyRdwT6L0Wy8CIKsPCPsDEpr75KX0kOin7-QTgza-kCK3byQcEPsOwoMYKB_kod5-Tgr41LsazTs6lB0FU3K1PZFd0uClusRa5j2TA5t7KhWtvGu-hK03jkfikYJsq-pAgiXQydrVqHk5CRlIMLZ8C4lNvQ9ObT4RZCBTvA3fmAxrgxHCrWZcYYImr0gvV_1fznPjyZfkk7qMmYe1siOBYCgwK-125EsPTW4lbBc70IGPpUumeOu9iMwTY2EYdscBcMPhCnn-8eOxWQ7n9E1kTZo1bQGS3qUG1qTeQ7Q-a0cgcjYbriR6YnNI_e2PgfcfV-i-_gTxwXiPsJ7R2nNFlXhfPIYa5Fj27YxtBIOPTssReL_FuRnflT6mnAVrdz4YCbUuOBCYWfwCVRTLA0ePKVNtkXlFu9IKlT6HIC7IipEA8kb4Z8_FFUq-c3WmeXeIneiOMgoFU5ayV9pj4Q7ALTjwWdHr4Pf1xpJ5Q.-CHCvIEF2gxx7ZpBC3pGEA',
-    'redirect_uri': 'https%3A%2F%2Flocalhost%3A3000'
+    'client_id': clientId,
+    'client_secret': clientSecret,
+    'code': code,
+     'redirect_uri': 'https%3A%2F%2Flocalhost%3A3000'
   }
 };
 
-let body;
-request(options, (error, response)=> {
-  if(error) console.log(error);
-  body = response.body;
-  res.send(body);
+const vehicleStatusOptions =  {
+  'method': 'POST',
+  'url': 'https://api.mps.ford.com/api/fordconnect/vehicles/v1/8a7f9fa878849d8a0179579d2f26043a/status',
+  'headers': {
+    'Accept': '*/*',
+    'Content-Type': 'application/json',
+    'Application-Id': applicationId,
+    'Authorization': 'Bearer null',
+    'api-version': '2020-06-01',
+    'callback-url': '{{callback-url}}'
+  },
+  formData: {
+
+  }
+};
+
+const vehicleInfoOptions = {
+  'method': 'GET',
+  'url': 'https://api.mps.ford.com/api/fordconnect/vehicles/v1/8a7f9fa878849d8a0179579d2f26043a',
+  'headers': {
+    'Application-Id': applicationId,
+    'Authorization': 'Bearer null',
+    'api-version': '2020-06-01'
+  }
+};
+
+const vehicleLocationOptions =  {
+  'method': 'POST',
+  'url': 'https://api.mps.ford.com/api/fordconnect/vehicles/v1/8a7f9fa878849d8a0179579d2f26043a/location',
+  'headers': {
+    'Application-Id': applicationId,
+    'Authorization': 'Bearer null',
+    'api-version': '2020-06-01'
+  }
+};
+
+const updateAccessToken = () => {
+  request(accessTokenOptions, function (error, response) {
+    if (error) throw new Error(error);
+    console.table(response.body);
+
+    //get access token first
+    accessToken = response.body.accessToken;
+});
+}
+
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname  +'/oauth.html');
+});
+
+app.get('/vehicle_info', (req, res) => {
+  
+  let vehicleInfo;
+
+  updateAccessToken();
+
+   //sending a post request for vehicle status as it is need for the get request to work
+   request(vehicleStatusOptions, (error, response) => {
+    if (error) throw new Error(error);
+    console.table(response.body);
+
+    //after sending post request, send get req for vehicl info
+    request(vehicleInfoOptions, (error, res) => {
+      if (error) throw new Error(error);
+      console.table(res.body);
+      vehicleInfo = res.body;
+    });
+ 
+    
+  });
+
+  res.send(JSON.stringify(vehicleInfo));
+} )
+
+//this retrieves the location of the vehicle from the api and sends it to the frontend
+app.get('/location', (req, res) => {
+  let location;
+  updateAccessToken();
+  request(vehicleLocationOptions, (error, response) => {
+    if (error) throw new Error(error);
+    console.table(response.body);
+    location = response.body;
+  });
+
+  res.send(JSON.stringify(location));
 })
 
-
-
-})
-
-
-
-app.listen(3000,()=> {
-    console.log("listening on port 3000");
+app.listen(3000, () =>{
+console.log("Listening");
 })
